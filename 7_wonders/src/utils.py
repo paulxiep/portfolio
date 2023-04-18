3593cf87910e620ad2440380b1fa6ecaf6eff41d
Paul <paulxieprc@gmail.com>
2023-04-18 13:46:24 +0700

    7 Wonders: Updated 2nd generation Deep-Q training results.

--- 7_wonders/src/utils.py
+++ 7_wonders/src/utils.py
@@ -7,12 +7,12 @@ def squash_idle(player_memory):
     choose_memory = []
     for i, memory in enumerate(player_memory):
         if memory[-1] == 0 and not isinstance(memory[0], str):
-            partial = [memory[0]], list(memory[7:]), memory[1]
+            partial = [memory[0]], list(memory[8:]), memory[1]
         elif memory[-1] == 1 and not isinstance(memory[1], str):
             action = [partial[2] + 80 * memory[1]]
         elif memory[-1] == 3:
-            choose_memory.append(partial[0] + action + [memory[2]] + list(memory[3:7]) + partial[1])
+            choose_memory.append(partial[0] + action + [memory[2]] + list(memory[3:8]) + partial[1])
 
-    for j in range(3, 7):
+    for j in range(3, 8):
         choose_memory[-1][j] = player_memory[-1][j]
     return choose_memory
